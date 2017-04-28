27a8fc33330fc085d88cff698b8f9e228d2cdc6d
Patrick Diehl <me@diehlpk.de>
2017-04-28 16:38:35 -0400

    Provide package and installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='gmshtoparticles',
+      version='0.1',
+      description='Transforms a .msh file generated with Gmsh to exodus 2 format',
+      url='https://github.com/IaPCS/gmsh-exodus-converter',
+      author='Patrick Diehl, Ilyass Tabiai',
+      author_email='me@diehlpk.de, ilyass.tabiai@gmail.com',
+      license='GPL-2.0',
+      packages=['gmshexodusconverter'],
+      zip_safe=False
+)
